a2e9630a78a8a2eebd0c3358d3b54cec738be850
SergeyKolybanov <kolybanov.sergei@yandex.ru>
2017-01-31 19:04:02 +0300

    Lesson 7

--- Snake/ConsoleApplication1/ConsoleApplication1/Point.cs
+++ Snake/ConsoleApplication1/ConsoleApplication1/Point.cs
@@ -11,6 +11,16 @@ namespace ConsoleApplication1
         public int x;
         public int y;
         public char sym;
+        public Point()
+        {
+            Console.WriteLine("Создаётся новая точка");
+        }
+        public Point (int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
--- Snake/ConsoleApplication1/ConsoleApplication1/Snake.cs
+++ Snake/ConsoleApplication1/ConsoleApplication1/Snake.cs
@@ -10,16 +10,44 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 2;
-            p1.sym = '*';
-            p1.Draw();
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-            p2.Draw();
+            Horizontal_line newline = new Horizontal_line(5,10,3,'-');
+
+            newline.Draw();
+            //Point p1 = new Point(1, 2, '*');
+            //p1.Draw();
+            //Point p2 = new Point(4, 5, '#');
+            //p2.Draw();
+            //Point p3 = new Point(5, 6, '$');
+            //p3.Draw();
+            //Point p4 = new Point(3, 4, '%');
+            //p4.Draw();
+            //Point p5 = new Point(6, 7, '^');
+            //p5.Draw();
+            //Console.WriteLine();
+            //Console.WriteLine("Вывод списка");
+            //List<Point> numlist = new List<Point>();
+            //numlist.Add(p1);
+            //numlist.Add(p2);
+            //numlist.Add(p3);
+            //numlist.Add(p4);
+            //numlist.Add(p5);
+            //Console.WriteLine("Cписок создан:");
+            //foreach (Point i in numlist)
+            //{
+            //    i.Draw();
+            //}
+            //numlist.Remove(p3);
+            //p3.Draw();
+            //Console.WriteLine();
+            //foreach (Point pointer in numlist)
+            //{
+            //    pointer.Draw();
+            //}
+            //numlist.RemoveAt(2);
+            //foreach (Point pointer in numlist)
+            //{
+            //    pointer.Draw();
+            //}
             Console.ReadLine();
         }
     }
